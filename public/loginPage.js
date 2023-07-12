6951addff87824d1479682c3e9d704badbfbf962
2d-man <v.belyakov1997@gmail.com>
2023-07-12 12:25:43 +0300

    Initial

---
+++ public/loginPage.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const userForm = new UserForm();
+
+userForm.loginFormCallback = ({login, password}) => {
+    let callback = (response) => {
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setLoginErrorMessage(response.error);
+        }
+    }
+
+    ApiConnector.login({login, password}, callback)
+    
+}
+
+userForm.registerFormCallback = ({login, password}) => {
+
+    let callback = (response) => {
+        if (response.success) {
+            location.reload();
+        } else {
+            userForm.setRegisterErrorMessage(response.error);
+        }
+    }
+
+    ApiConnector.register({login, password}, callback)
+}
